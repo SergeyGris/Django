b8b07678d6a8ab4e9a79a03c0c0a9e7c57013b9c
SergeyG <svg95@list.ruG>
2022-12-27 21:52:46 +0300

    lesson_4 News model

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,19 @@ DATABASES = {
     }
 }
 
+# Это возможный вариант конфигурации для СУБД PostgreSQL. Его добавлять в проект не надо.
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
